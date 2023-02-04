e526e94a94fae6fd25380e49a726f2acb6e3b3f0
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-02-04 16:33:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const dbConnect = require('./config/dbConnect');
+const app = express();
+require('dotenv').config();
+const { notFound, errorHandler } = require("./middlewares/errorHandler")
+const PORT = process.env.PORT || 4000;
+const authRouter = require('./routes/authRoute');
+dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use("/api/user", authRouter);
+
+
+
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running PORT ${PORT}`)
+});
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,23 @@
+// note Found
+
+const notFound = (req, res, next) => {
+    const error = new Error(`Note Found: ${req.originalUrl}`);
+    res.statatus(404);
+    next(error);
+};
+
+//ErroHandler
+
+const errorHandler = (err, req, res, next) => {
+    const statuscode = res.statusCode == 200 ? 500 : res.statusCode;;
+    res.status(statuscode)
+    res.json({
+        messege: err?.messege,
+        stack: err?.stack
+    })
+}
+
+module.exports={
+    notFound,
+    errorHandler
+}
