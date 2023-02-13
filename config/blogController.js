8ff98e4b5b7ef6f912b2019795e64e3ba790a493
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-02-13 17:20:24 +0700

    finish CRUD Blog

---
+++ config/blogController.js
@@ -0,0 +1,66 @@
+const Blog = require('../models/blogModel');
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const validateMongodbId = require("../utils/validateMongodbId");
+
+
+const createBlog = asyncHandler(async (req, res) => {
+    try {
+        const newBlog = await Blog.create(req.body);
+        res.json(newBlog);
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new: true
+        });
+        res.json(updateBlog);
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+const getBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const getBlog = await Blog.findById(id);
+        const updateViews = await Blog.findByIdAndUpdate(id, {
+            $inc: { numViews: 1 }
+        },
+            { new: true }
+        );
+        res.json(updateViews);
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+const getAllBlog = asyncHandler(async (req, res) => {
+    try {
+        const getBlogs = await Blog.find();
+        res.json(getBlogs);
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteBlog = await Blog.findByIdAndDelete(id);
+        res.json(deleteBlog);
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+module.exports = {
+    createBlog, updateBlog,
+    getBlog, getAllBlog,
+    deleteBlog
+}
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { notFound, errorHandler } = require("./middlewares/errorHandler")
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
+const blogRouter = require('./routes/blogRoute');
 const cookieParser = require("cookie-parser");
 const morgan = require('morgan');
 dbConnect();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
 
 app.get("/api", (req, res) => {
   res.json({ "users": ["userone", "usertwo"] })
