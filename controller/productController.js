9e5b7581891d2837cef4a981ef455a032a707cac
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-02-14 15:58:23 +0700

    update rating

--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel');
+const User = require('../models/userModel');
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 const validateMongoDbId = require("../utils/validateMongodbId");
@@ -102,10 +103,102 @@ const getAllProduct = asyncHandler(async (req, res) => {
     }
 });
 
+const addToWishlist = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { prodId } = req.body;
+    try {
+        const user = await User.findById(_id);
+        const alreadyadded = user.wishlist.find((id) => id.toString() === prodId);
+        if (alreadyadded) {
+            let user = await User.findByIdAndUpdate(
+                _id,
+                {
+                    $pull: { wishlist: prodId },
+                },
+                {
+                    new: true,
+                }
+            );
+            res.json(user);
+        } else {
+            let user = await User.findByIdAndUpdate(
+                _id,
+                {
+                    $push: { wishlist: prodId },
+                },
+                {
+                    new: true,
+                }
+            );
+            res.json(user);
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const rating = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { star, prodId, comment } = req.body;
+    try {
+        const product = await Product.findById(prodId);
+        let alreadyReated = product.ratings.find(
+            (userId) => userId.postedby.toString() === _id.toString()
+        );
+        if (alreadyReated) {
+            const updateRating = await Product.updateOne(
+                {
+                    ratings: { $elemMatch: alreadyReated }
+                },
+                {
+                    $set: { "ratings.$.star": star, "ratings.$.comment": comment }
+                },
+                {
+                    new: true
+                }
+            );
+            res.json(updateRating)
+        } else {
+            const rateProduct = await Product.findByIdAndUpdate(prodId, {
+                $push: {
+                    ratings: {
+                        star: star,
+                        comment: comment,
+                        postedby: _id
+                    }
+                }
+            },
+                {
+                    new: true
+                }
+            );
+            res.json(rateProduct);
+        }
+        const getallratings = await Product.findById(prodId);
+        let totalRating = getallratings.ratings.length;
+        let ratingsum = getallratings.ratings
+            .map((item) => item.star)
+            .reduce((prev, curr) => prev + curr, 0);
+        let actualRating = Math.round(ratingsum / totalRating);
+        let finalproduct = await Product.findByIdAndUpdate(
+            prodId,
+            {
+                totalrating: actualRating,
+            },
+            { new: true }
+        );
+        res.json(finalproduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
 module.exports = {
     createProduct,
     getAProduct,
     getAllProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    addToWishlist,
+    rating
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,13 +3,19 @@ const { createProduct,
     getAProduct,
     getAllProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    addToWishlist,
+    rating
 } = require('../controller/productController');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createProduct);
+
 router.get("/:id", getAProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 router.get("/", getAllProduct);
